83a05eea2b8155307d5d96e25463ae17835f0f53
lodenrogue <phyzix5761@gmail.com>
2023-07-15 18:54:48 -0400

    Added printing logs

--- exlogs.py
+++ exlogs.py
@@ -1,7 +1,15 @@
+#! /usr/bin/env python3
+
 from datetime import datetime
 import argparse
+import sys
 
 EXERCISE_LOGS_FILE_PATH = "."
+
+
+def print_logs():
+    with open(f'{EXERCISE_LOGS_FILE_PATH}/exercise_logs.csv', "r") as f:
+        print(f.read().strip())
 
 
 def log_exercise(minutes, description):
@@ -11,12 +19,15 @@ def log_exercise(minutes, description):
 
 
 def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("minutes", help="exercise minutes")
-    parser.add_argument("-d", default="", help="excercise description")
+    if len(sys.argv) == 1:
+        print_logs()
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("minutes", help="exercise minutes")
+        parser.add_argument("-d", default="", help="excercise description")
 
-    args = parser.parse_args()
-    log_exercise(args.minutes, args.d)
+        args = parser.parse_args()
+        log_exercise(args.minutes, args.d)
 
 
 if __name__ == '__main__':
