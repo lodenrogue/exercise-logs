cfdffebee71d169267f1009a7423cc65fe3cc59e
lodenrogue <phyzix5761@gmail.com>
2023-07-15 18:47:38 -0400

    Added argparse

--- exlogs.py
+++ exlogs.py
@@ -1,21 +1,23 @@
-import sys
 from datetime import datetime
+import argparse
 
 EXERCISE_LOGS_FILE_PATH = "."
 
 
-def log_exercise(minutes):
+def log_exercise(minutes, description):
     with open(f'{EXERCISE_LOGS_FILE_PATH}/exercise_logs.csv', "a") as f:
         date = datetime.now().strftime("%Y-%m-%d")
-        f.write(f'{date},{minutes}\n')
+        f.write(f'{date},{minutes},"{description}"\n')
 
 
 def run():
-    if len(sys.argv) < 2:
-        print("Usage: exlogs <minutes>")
-        exit(1)
-    else:
-        log_exercise(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("minutes", help="exercise minutes")
+    parser.add_argument("-d", default="", help="excercise description")
+
+    args = parser.parse_args()
+    log_exercise(args.minutes, args.d)
+
 
 if __name__ == '__main__':
     run()
